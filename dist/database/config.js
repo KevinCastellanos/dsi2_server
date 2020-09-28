9ce7922726c948e29a7eabf82bc638b7a0326d3a
KevinCastellanos <kevincastellanosgaray@gmail.com>
2020-09-28 00:35:09 -0400

    dist

---
+++ dist/database/config.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// conexion a mysql
+const mysql = require('mysql');
+// configuracion de base de datos
+// Create a MySQL pool
+// Para la eficiencia, vamos a crear un pool de MySQL, 
+// que nos permite utilizar múltiples conexiones a la vez en lugar de tener 
+// que manualmente abrir y cerrar conexiones múltiples.
+// Por último, a exportar la piscina de MySQL para poder utilizar la aplicación.
+const config = {
+    host: 'ec2-18-222-41-77.us-east-2.compute.amazonaws.com',
+    user: 'tpi115',
+    password: 'Tpi115.$',
+    database: 'dsi2',
+    connectTimeout: 20000,
+    acquireTimeout: 20000
+};
+// exportamos la conexion establecidas
+exports.pool = mysql.createPool(config);
--- dist/routes/router.js
+++ dist/routes/router.js
@@ -1,67 +1,63 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // archivo destinado a crear los api Resfull
-import { Router, Request, Response } from 'express';
-import Server from '../class/server';
-import { usuarioConectados } from '../sockets/socket';
-import * as mysql from '../database/sql';
-
-
+const express_1 = require("express");
+const server_1 = __importDefault(require("../class/server"));
+const socket_1 = require("../sockets/socket");
+const mysql = __importStar(require("../database/sql"));
 //exportamos la constante router
-export const router  = Router();
-
-
-
-router.get('/mensajes', (req: Request, res: Response) => {
+exports.router = express_1.Router();
+exports.router.get('/mensajes', (req, res) => {
     res.json({
         ok: true,
         message: 'todo esta bien'
     });
 });
-
-router.post('/mensajes', (req: Request, res: Response) => {
+exports.router.post('/mensajes', (req, res) => {
     // leer la informacion que estoy recibiendo desde el cliente
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
-
     const payload = {
         cuerpo,
         de
-    }
-
-    const server  = Server.instance;
-
+    };
+    const server = server_1.default.instance;
     server.io.emit('mensaje-nuevo', payload);
-
     res.json({
         ok: true,
         cuerpo,
         de
     });
 });
-
 // nueva ruta con parametros
-router.post('/mensajes/:id', (req: Request, res: Response) => {
+exports.router.post('/mensajes/:id', (req, res) => {
     // leer la informacion que estoy recibiendo desde el cliente
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
     // obtener el id
     const id = req.params.id;
-
     const payload = {
         de,
         cuerpo
     };
-
     // Aqui tenemos que agregar nuestro servicio rest con el servidor de sockets
     // para que la app obtenga los mensaje en tiempo real
     // declaramos la instancia de nuestro server
-
     // como usa el patron singleton, es la misma instancia del servidor de sockets corriendo
-    const server = Server.instance;
-
+    const server = server_1.default.instance;
     // nos vamos a referir a nuesro servidor de sockets
     // el in sirve para enviar mensaje a un cliente en una canal en paticular
-    server.io.in( id ).emit('mensaje-privado', payload);
-
+    server.io.in(id).emit('mensaje-privado', payload);
     res.json({
         ok: true,
         cuerpo,
@@ -69,88 +65,67 @@ router.post('/mensajes/:id', (req: Request, res: Response) => {
         id
     });
 });
-
 // Servicios para obtener todos los IDs de los usuarios
-router.get('/usuarios', (req: Request, res: Response) => {
-
+exports.router.get('/usuarios', (req, res) => {
     // usamos la insancia de ioSockets para obtener los id conectados
-    const server = Server.instance;
-    server.io.clients( (err: any, clientes: string[])=> {
-        if(err) {
+    const server = server_1.default.instance;
+    server.io.clients((err, clientes) => {
+        if (err) {
             return res.json({
                 ok: false,
                 err
             });
         }
-
         res.json({
             ok: true,
             clientes
         });
     });
 });
-
 // obtenemos los nombre de los departamenos sin filtro
-router.get('/obtener-departamentos', (req: Request, res: Response) => {
-    
+exports.router.get('/obtener-departamentos', (req, res) => {
     const consultaSQL = `SELECT *
                         FROM DEPARTAMENTO;`;
-
-        mysql.query(consultaSQL).then( (data: any) => {
-            // data: retorna un array de objetos (si tiene objetos sino mandara un array vacio)
-
-
-            // respondemos al cliente si es exito
-            res.json(data);
-        
-        }).catch( (err: any) => {
-
-            // respondemos al cliente que hay error
-            res.status(500).json({ 
-                err 
-            });
+    mysql.query(consultaSQL).then((data) => {
+        // data: retorna un array de objetos (si tiene objetos sino mandara un array vacio)
+        // respondemos al cliente si es exito
+        res.json(data);
+    }).catch((err) => {
+        // respondemos al cliente que hay error
+        res.status(500).json({
+            err
         });
-    
+    });
 });
-
 //Api Login
-router.post('/login', (req: Request, res: Response) => {
+exports.router.post('/login', (req, res) => {
     console.log(req.body);
-
     // query: viene concatenado en la url
     // body: los parametros no vienen en la url
-    
-
     let consultaSQL = `SELECT * 
                     FROM USUARIO 
                     WHERE NOMBRE = '${req.body.usuario}'
                     AND CONTRASEÑA = '${req.body.password}';`;
-
     // consulta estructurada con promesas
-    mysql.query(consultaSQL).then( (data: any) => {
+    mysql.query(consultaSQL).then((data) => {
         res.json(data[0]);
-    }).catch( (err) => {
+    }).catch((err) => {
         res.status(500).json({ err });
     });
 });
-
 // obtener usuarios y sus nombres
-router.get('/usuarios/detalle', (req: Request, res: Response) => {
-
+exports.router.get('/usuarios/detalle', (req, res) => {
     res.json({
         ok: true,
-        clientes: usuarioConectados.getLista()
+        clientes: socket_1.usuarioConectados.getLista()
     });
 });
-
 // api obtener detalle abono
-router.post('/obtener-abonos-cliente', (req: Request, res: Response) => {
-    
+exports.router.post('/obtener-abonos-cliente', (req, res) => {
     // NOTA: esta consulta sql está estructurada para leer datos de 3 tablas diferentes
     // tabla a: independiente = es la tabla de expediente
     // tabla b: independiente = es la tabla pago
     // tabla c: dependiente = es la tabla detallepago que une datos entre a y c
-    
     const consultaSQL = `SELECT a.*, b.*,
                                         (
                                             (
@@ -167,62 +142,48 @@ router.post('/obtener-abonos-cliente', (req: Request, res: Response) => {
                                          ) as 'detalle'
                         FROM EXPEDIENTE a, PAGO b
                         WHERE a.IDEXPEDIENTE = ${req.body.id_expediente};`;
-
-        mysql.query(consultaSQL).then( (data: any) => {
-            // data: retorna un array de objetos (si tiene objetos sino mandara un array vacio)
-            // respondemos al cliente si es exito
-
-            // mysql retorna un arreglo dentro de otro arreglo el ultimo lo retorna en forma de cadena
-            // para eso vamos a convertirlo a un objeto json (este es un caso especial cuando ocupamos unicamente json_arrayg)
-            for (var i = 0; i < data.length; i++) {
-                // parseamos el array que recibimos en cadena a objeto y lo volvemos a asignar a la misma variable
-                data[i].detalle = JSON.parse(data[i].detalle);
-            }
-
-            // retornamos solo el objeto para mayor facilidad de manipulacion de datos
-            res.json(data[0]);
-        
-        }).catch( (err: any) => {
-
-            // respondemos al cliente que hay error
-            res.status(500).json({ 
-                err 
-            });
+    mysql.query(consultaSQL).then((data) => {
+        // data: retorna un array de objetos (si tiene objetos sino mandara un array vacio)
+        // respondemos al cliente si es exito
+        // mysql retorna un arreglo dentro de otro arreglo el ultimo lo retorna en forma de cadena
+        // para eso vamos a convertirlo a un objeto json (este es un caso especial cuando ocupamos unicamente json_arrayg)
+        for (var i = 0; i < data.length; i++) {
+            // parseamos el array que recibimos en cadena a objeto y lo volvemos a asignar a la misma variable
+            data[i].detalle = JSON.parse(data[i].detalle);
+        }
+        // retornamos solo el objeto para mayor facilidad de manipulacion de datos
+        res.json(data[0]);
+    }).catch((err) => {
+        // respondemos al cliente que hay error
+        res.status(500).json({
+            err
         });
-    
+    });
 });
-
 // api para registrar abno de cliente
-router.post('/registrar-abono-cliente', (req: Request, res: Response) => {
+exports.router.post('/registrar-abono-cliente', (req, res) => {
     // query: viene concatenado en la url
     // body: los parametros no vienen en la url
-    
-
-    let consultaSQL =  `INSERT INTO DETALLEPAGOS (IDPAGO ,IDEXPEDIENTE, FECHAPAGO, ABONO, SALDO) 
+    let consultaSQL = `INSERT INTO DETALLEPAGOS (IDPAGO ,IDEXPEDIENTE, FECHAPAGO, ABONO, SALDO) 
                         VALUES (${req.body.id_pago}, ${req.body.id_expediente}, '${req.body.fecha}', ${req.body.abono}, ${req.body.saldo});`;
-
     // consulta estructurada con promesas
-    mysql.query(consultaSQL).then( (data: any) => {
+    mysql.query(consultaSQL).then((data) => {
         res.json(data);
-    }).catch( (err) => {
+    }).catch((err) => {
         res.status(500).json({ err });
     });
 });
-
-router.post('/obtener-clientes', (req: Request, res: Response) => {
+exports.router.post('/obtener-clientes', (req, res) => {
     // query: viene concatenado en la url
     // body: los parametros no vienen en la url
-
-    let consultaSQL =  `SELECT a.*, b.IDEXPEDIENTE FROM CLIENTE as a, EXPEDIENTE as b
+    let consultaSQL = `SELECT a.*, b.IDEXPEDIENTE FROM CLIENTE as a, EXPEDIENTE as b
                         WHERE a.IDCLIENTE = b.IDCLIENTE`;
-
     // consulta estructurada con promesas
-    mysql.query(consultaSQL).then( (data: any) => {
+    mysql.query(consultaSQL).then((data) => {
         // caso de exito
         res.json(data);
-    }).catch( (err) => {
+    }).catch((err) => {
         // caso de error
         res.status(500).json({ err });
     });
 });
-
